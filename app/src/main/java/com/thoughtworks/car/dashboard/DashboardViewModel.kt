ed2d88da5ffa9932aa3f523a4880d3d5d19fdcea
CHEN Yang <yangchen@thoughtworks.com>
2023-04-06 14:24:08 +0800

    feat: implement engine ignition button logic

--- app/src/main/java/com/thoughtworks/car/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/thoughtworks/car/dashboard/DashboardViewModel.kt
@@ -6,6 +6,7 @@ import com.thoughtworks.car.dashboard.ui.door.DoorUseCase
 import com.thoughtworks.car.dashboard.ui.hvac.HvacUseCase
 import com.thoughtworks.car.dashboard.ui.media.MediaUseCase
 import com.thoughtworks.car.dashboard.ui.navi.NaviUseCase
+import com.thoughtworks.car.dashboard.ui.ignition.IgnitionUseCase
 import com.thoughtworks.car.dashboard.ui.status.StatusUseCase
 import com.thoughtworks.car.dashboard.ui.time.TimeUseCase
 import com.thoughtworks.car.dashboard.ui.voice.VoiceUseCase
@@ -16,6 +17,7 @@ import javax.inject.Inject
 class DashboardViewModel @Inject constructor(
     val timeUseCase: TimeUseCase,
     val voiceUseCase: VoiceUseCase,
+    val ignitionUseCase: IgnitionUseCase,
     val doorUseCase: DoorUseCase,
     val hvacUseCase: HvacUseCase,
     val naviUseCase: NaviUseCase,
