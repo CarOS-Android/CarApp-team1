40655a32b748341eb0e5cf6ed1244c6fa7135b96
CHEN Yang <yangchen@thoughtworks.com>
2023-04-17 15:49:55 +0800

    feat: implement air air condition control view ui

--- app/src/main/java/com/thoughtworks/car/vehicle/ui/aircondition/AirConditionControlUseCase.kt
+++ app/src/main/java/com/thoughtworks/car/vehicle/ui/aircondition/AirConditionControlUseCase.kt
@@ -1,9 +1,19 @@
 package com.thoughtworks.car.vehicle.ui.aircondition
 
 import com.thoughtworks.car.core.ui.BaseUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
+data class AirConditionControlUiState(
+    val fragranceState: Boolean = false,
+)
+
 class AirConditionControlUseCase @Inject constructor() : BaseUseCase {
+    private val _uiState: MutableStateFlow<AirConditionControlUiState> =
+        MutableStateFlow(AirConditionControlUiState())
+    val uiState: StateFlow<AirConditionControlUiState> = _uiState
+
     override fun onCleared() {
         // do nothing
     }
